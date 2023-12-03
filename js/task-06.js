178dedaffc78778a2a0056bbcdd00e5d271e1e5b
YuliiaaLevchenko <141173167+YuliiaaLevchenko@users.noreply.github.com>
2023-12-03 16:08:39 +0200

    finish upd

--- js/task-06.js
+++ js/task-06.js
@@ -3,3 +3,39 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const controls = document.getElementById('controls');
+const input = controls.querySelector('input');
+const createButton = controls.querySelector('[data-create]');
+const destroyButton = controls.querySelector('[data-destroy]');
+const boxesContainer = document.getElementById('boxes');
+
+createButton.addEventListener('click', createBoxes);
+destroyButton.addEventListener('click', destroyBoxes);
+
+function createBoxes() {
+  const amount = input.valueAsNumber;
+
+  if (amount >= 1 && amount <= 100) {
+    const fragment = document.createDocumentFragment();
+
+    for (let i = 0; i < amount; i++) {
+      const box = document.createElement('div');
+      const size = 30 + i * 10;
+
+      box.style.width = `${size}px`;
+      box.style.height = `${size}px`;
+      box.style.backgroundColor = getRandomHexColor();
+
+      fragment.appendChild(box);
+    }
+    boxesContainer.appendChild(fragment);
+    input.value = ''; 
+  } else {
+    alert('Please enter a number between 1 and 100.');
+  }
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = ''; 
+}
